d3ffc297f73182e2d13d2d6bf08f7695d9031e5b
术生何 <urfdvw@gmail.com>
2023-10-06 11:46:54 -0400

    drag and drop test

--- docs/logs.md
+++ docs/logs.md
@@ -2,3 +2,8 @@
 - `npm install @mui/material @emotion/react @emotion/styled`
 - `npm install vite-plugin-singlefile`
 - `npm install gh-pages`
+
+# doc
+- top bar
+    - https://mui.com/material-ui/react-breadcrumbs/
+- drag and drop example
--- src/FolderView.jsx
+++ src/FolderView.jsx
@@ -7,8 +7,27 @@ function BasicBreadcrumbs() {
     return (
         <>
             <Breadcrumbs aria-label="breadcrumb">
-                <Button> hi </Button>
-                <Button> there </Button>
+                <Button
+                    onDrop={(event) => {
+                        console.log("onDrop");
+                        console.log(event);
+                        console.log(JSON.parse(event.dataTransfer.getData("data")));
+                    }}
+                    onDragOver={(event) => {
+                        event.preventDefault(); // to allow drop
+                    }}
+                >
+                    hi
+                </Button>
+                <Button
+                    draggable={true}
+                    onDragStart={(event) => {
+                        event.dataTransfer.setData("data", JSON.stringify({ hi: 1 })); // data has to be string
+                    }}
+                >
+                    {" "}
+                    there{" "}
+                </Button>
                 <Button> what's up </Button>
             </Breadcrumbs>
         </>
