94b01f9cc1748e891dad14206aa55f6e3ab69470
River Wang <urfdvw@gmail.com>
2023-10-07 11:09:46 -0400

    folder content list

--- docs/logs.md
+++ docs/logs.md
@@ -1,5 +1,5 @@
 # packages
-- `npm install @mui/material @emotion/react @emotion/styled`
+- `npm install @mui/material @emotion/react @emotion/styled @mui/icons-material`
 - `npm install vite-plugin-singlefile`
 - `npm install gh-pages`
 
@@ -7,3 +7,7 @@
 - top bar
     - https://mui.com/material-ui/react-breadcrumbs/
 - drag and drop example
+- content list 
+    - https://mui.com/material-ui/react-list/
+- icons
+    - https://mui.com/material-ui/material-icons/
--- src/FolderView.jsx
+++ src/FolderView.jsx
@@ -2,6 +2,14 @@ import * as React from "react";
 import Typography from "@mui/material/Typography";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import { Button } from "@mui/material";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import FolderIcon from '@mui/icons-material/Folder';
+import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
 
 function BasicBreadcrumbs() {
     return (
@@ -30,6 +38,35 @@ function BasicBreadcrumbs() {
                 </Button>
                 <Button> what's up </Button>
             </Breadcrumbs>
+            <Divider />
+            <List>
+                <ListItem disablePadding>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <FolderIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Inbox" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <InsertDriveFileIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Drafts" />
+                    </ListItemButton>
+                </ListItem>
+            </List>
+            {/* <List dense={dense}>
+                {generate(
+                    <ListItem>
+                        <ListItemIcon>
+                            <FolderIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Single-line item" secondary={secondary ? "Secondary text" : null} />
+                    </ListItem>
+                )}
+            </List> */}
         </>
     );
 }
