4e2f78e78e0f55f26e0f2f6e38bc620e4b07a1b9
River Wang <urfdvw@gmail.com>
2023-11-28 09:01:39 -0500

    sort of working

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,77 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import Button from "@mui/material/Button";
 import jsonSchemaDefaults from "json-schema-defaults";
 import global_config_schema from "./schemas/global.json";
 import editor_config_schema from "./schemas/editor.json";
 
-function useConfig(schemas, config_prefix = "config_") {
-    const [config_changed, set_config_changed] = useState(false);
-    useEffect(() => {
-        for (const schema_name in schemas) {
-            var config_values = jsonSchemaDefaults(schemas[schema_name]);
-            var config_values_local = get_config(schema_name);
-            console.log(config_values, config_values_local);
-            if (config_values_local !== null && typeof config_values_local === typeof {}) {
-                for (const field_name in config_values) {
-                    if (field_name in config_values_local) {
-                        config_values[field_name] = config_values_local[field_name];
-                    }
-                }
+// ---- Util ----
+function isDefined(obj) {
+    return obj !== null && obj != undefined;
+}
+
+function isObject(obj) {
+    return typeof obj === typeof {};
+}
+
+// ---- localStorage interface
+function dumpLocalStorage() {
+    // https://codereview.stackexchange.com/a/273991
+    return Object.keys(localStorage).reduce((obj, k) => ({ ...obj, [k]: JSON.parse(localStorage.getItem(k)) }), {});
+}
+
+function useLocalStorage() {
+    const [localStorageState, _setLocalStorageState] = useState({});
+    function initLocalStorageState() {
+        _setLocalStorageState(dumpLocalStorage());
+    }
+
+    function setLocalStorageState(name, value) {
+        localStorage.setItem(name, JSON.stringify(value));
+        _setLocalStorageState(dumpLocalStorage());
+    }
+
+    return { localStorageState, setLocalStorageState, initLocalStorageState };
+}
+
+// ---- config interface ----
+function getConfigWithDefaults(current_config, schema) {
+    var config = jsonSchemaDefaults(schema);
+    if (isDefined(current_config) && isObject(current_config)) {
+        for (const field_name in config) {
+            if (field_name in current_config) {
+                config[field_name] = current_config[field_name];
             }
-            set_config(schema_name, config_values);
-            localStorage.setItem(config_prefix + schema_name, JSON.stringify(config_values));
         }
-    }, []);
+    }
+    return config;
+}
+
+function useConfig(schemas, config_prefix = "config_") {
+    const { localStorageState, setLocalStorageState, initLocalStorageState } = useLocalStorage();
+    const [initStep, setInitStep] = useState(0);
+
+    useEffect(() => {
+        if (initStep === 0) {
+            console.log("init step 0");
+            initLocalStorageState();
+            setInitStep(1);
+        }
+        if (initStep === 1) {
+            console.log("init step 1");
+            for (const schema_name in schemas) {
+                var config_values = getConfigWithDefaults(get_config(schema_name), schemas[schema_name]);
+                set_config(schema_name, config_values);
+            }
+        }
+    }, [initStep]);
 
     function get_config(schema_name) {
-        return JSON.parse(localStorage.getItem(config_prefix + schema_name));
+        const config = localStorageState[config_prefix + schema_name];
+        return isDefined(config) ? config : null;
     }
 
     function set_config(schema_name, config_values) {
-        localStorage.setItem(config_prefix + schema_name, JSON.stringify(config_values));
-        set_config_changed(true);
-    }
-
-    function get_config_field(schema_name, field_name) {
-        const config = get_config(schema_name);
-        if (field_name in config) {
-            return config[config];
-        } else {
-            console.error("no field called " + field_name + " in config schema " + schema_name);
-        }
+        setLocalStorageState(config_prefix + schema_name, config_values);
     }
 
     function set_config_field(schema_name, field_name, field_value) {
@@ -62,19 +94,18 @@ function useConfig(schemas, config_prefix = "config_") {
         }
     }
 
-    return { set_config, get_config, set_config_field, get_config_field };
+    return { config: localStorageState, set_config, set_config_field };
 }
 
 function App() {
-    const { set_config, get_config, set_config_field, get_config_field } = useConfig({
+    const { config, set_config, set_config_field } = useConfig({
         global: global_config_schema,
         editor: editor_config_schema,
     });
 
     return (
         <>
-            <p>{JSON.stringify(get_config("editor"))}</p>
-            <p>{JSON.stringify(get_config("global"))}</p>
+            <p>{JSON.stringify(config)}</p>
             <button
                 onClick={() => {
                     set_config_field("editor", "font", 10);
@@ -88,6 +119,37 @@ function App() {
                 }}
             >
                 set font 14
+            </button>
+            <br />
+            <button
+                onClick={() => {
+                    set_config_field("editor", "nonsense", 10);
+                }}
+            >
+                set nonsense 10
+            </button>
+            <button
+                onClick={() => {
+                    localStorage.setItem("config_editor", "10");
+                    console.log(localStorage);
+                }}
+            >
+                set nonsense 10 manual
+            </button>
+            <br />
+            <button
+                onClick={() => {
+                    localStorage.clear();
+                }}
+            >
+                clear
+            </button>
+            <button
+                onClick={() => {
+                    console.log(localStorage);
+                }}
+            >
+                log localStorage
             </button>
         </>
     );
