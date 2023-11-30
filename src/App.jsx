b09fe9da657174604e6cd465d660131c6ce983e0
River Wang <urfdvw@gmail.com>
2023-11-30 09:26:14 -0500

    dark example

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,27 @@ import { useConfig, ConfigForms } from "./react-localstorage-config";
 import "./App.css";
 import global_config_schema from "./schemas/global.json";
 import editor_config_schema from "./schemas/editor.json";
+import DarkTheme from "react-lazy-dark-theme";
 
 function App() {
     const schemas = [global_config_schema, editor_config_schema];
     const { config, set_config } = useConfig(schemas);
+
     console.log(config);
-    return <ConfigForms schemas={schemas} config={config} set_config={set_config} />;
+    var dark = null;
+    try {
+        if (config.global.theme === "system") {
+            dark = null;
+        } else if (config.global.theme === "dark") {
+            dark = true;
+        }
+    } catch {}
+    return (
+        <>
+            <DarkTheme dark={dark} />
+            <ConfigForms schemas={schemas} config={config} set_config={set_config} />
+        </>
+    );
 }
 
 export default App;
